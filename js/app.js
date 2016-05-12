07063fd183fc5a56e00766addf6e610f4e051b2c
scmac00 <steve@Stevens-MacBook-Pro.local>
2016-05-12 00:43:25 -0600

    Tab functionality
    Roughed in tab jQuery code

--- js/app.js
+++ js/app.js
@@ -3,17 +3,39 @@ $(document).ready(function(){
 
 	var tabs = $('.tabs');
 
-	tabs.find('.tab-content').hide();
+	var tabContent = tabs.find('.tab-content').hide();
 	
+	var tab = tabs.find('.tab');
+
+	tabContent.first().show();
+
+	tab.on('click', function() {
+		var $this = $(this);
+		var tabNum = $this.index();
+
+		if ($this.hasClass('active')) {
+			//do nothing
+		}
+		else {
+			tab.removeClass('active');
+			$this.addClass('active');
+
+			tabContent.fadeOut();
+			tabContent.eq(tabNum).fadeIn();
+		}
+
+
+	});
+
 	
 });
 
-.tabs {
-    .tab-links {
-		a.tab {
-	}
+// .tabs {
+//     .tab-links {
+// 		a.tab {
+// 	}
 	
-    .tab-content {
-        padding: $spacing-unit/2;
-    }
-}
+//     .tab-content {
+//         padding: $spacing-unit/2;
+//     }
+// }
