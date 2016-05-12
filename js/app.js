9a86f48744fdc3a5319256501c9b2c15bd4a9ede
scmac <s-mac@shaw.ca>
2016-05-12 00:06:23 -0600

    Equipment Finder
    More updates

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 
 $(document).ready(function(){
+
+	var tabs = $('.tabs');
+
+	tabs.find('.tab-content').hide();
+	
 	
 });
+
+.tabs {
+    .tab-links {
+		a.tab {
+	}
+	
+    .tab-content {
+        padding: $spacing-unit/2;
+    }
+}
